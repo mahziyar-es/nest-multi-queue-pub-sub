fd68613edd5b47ff30383d8f51cc7f1d14e282c1
Mahziyar Esmaili <mahziyar.esmaili.dev@gmail.com>
2024-07-16 18:15:59 +0200

    import Queues module in app module

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { QueuesModule } from './queues/queues.module';
 
 @Module({
-  imports: [],
+  imports: [QueuesModule],
   controllers: [],
   providers: [],
 })
